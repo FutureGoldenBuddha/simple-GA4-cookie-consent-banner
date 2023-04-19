d4e245d9fd7e6d8c7ef9c69b40c112bd56b31e03
FutureGoldenBuddha <128430541+FutureGoldenBuddha@users.noreply.github.com>
2023-04-19 11:02:11 +0100

    Create gtag-init.js

---
+++ gtag-init.js
@@ -0,0 +1,14 @@
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('consent', 'default', {
+  'ad_storage': 'denied',
+  'analytics_storage': 'denied'
+});
+
+gtag('config', 'G-YOUR_ID'); // Remember to replace G-YOUR_ID with your actual GA id.
